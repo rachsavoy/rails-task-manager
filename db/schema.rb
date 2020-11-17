bcd4211f40353996abe07b404fe4637b93183c12
Rachael Savoy <rachael.a.hakim@gmail.com>
2020-11-17 12:42:02 +0900

    ARCH: Generated rails model for Task.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_032648) do
+ActiveRecord::Schema.define(version: 2020_11_17_033739) do
 
   create_table "restaurants", force: :cascade do |t|
+    t.string "title"
+    t.text "details"
+    t.boolean "completed"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
     t.boolean "completed"
